c2106ea01d5cfd4c8ca283420fed7f393130fba7
Douglas Leal <douglas.leal@live.com>
2023-01-06 09:58:31 -0300

    Readme criado e favicon adicionado

--- js/API/Api.js
+++ js/API/Api.js
@@ -1,6 +1,5 @@
 class Api{
-    //static url_api = "https://xp41-soundgarden-api.herokuapp.com";
-    static url_api = "http://localhost:3000";
+    static url_api = "https://xp41-soundgarden-api.herokuapp.com";
 }
 
 export default Api;
